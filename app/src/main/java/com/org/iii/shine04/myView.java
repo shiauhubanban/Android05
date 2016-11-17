9437fb2e4772d001d03d316c8eb60fef9c8adaec
user <shiauhubanban>
2016-11-17 10:05:29 +0800

    v2

---
+++ app/src/main/java/com/org/iii/shine04/myView.java
@@ -0,0 +1,18 @@
+package com.org.iii.shine04;
+
+import android.content.Context;
+import android.graphics.Color;
+import android.util.AttributeSet;
+import android.view.View;
+
+/**
+ * Created by user on 2016/11/17.
+ */
+
+public class myView extends View {
+    public myView(Context context, AttributeSet attrs) {
+        super(context, attrs);
+    setBackgroundColor(Color.GREEN);
+    }
+
+}
