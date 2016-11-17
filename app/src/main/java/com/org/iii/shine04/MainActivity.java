9b8dabf0789bce70c3a1862d9e0acb071d3cbff2
user <shiauhubanban>
2016-11-17 10:46:01 +0800

    v5

--- app/src/main/java/com/org/iii/shine04/MainActivity.java
+++ app/src/main/java/com/org/iii/shine04/MainActivity.java
@@ -2,12 +2,21 @@ package com.org.iii.shine04;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
-
+    private Button clear;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        clear = (Button)findViewById(R.id.clear);
+        clear.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                
+            }
+        });
     }
 }
--- app/src/main/java/com/org/iii/shine04/myView.java
+++ app/src/main/java/com/org/iii/shine04/myView.java
@@ -5,6 +5,8 @@ import android.graphics.Canvas;
 import android.graphics.Color;
 import android.graphics.Paint;
 import android.util.AttributeSet;
+import android.util.Log;
+import android.view.MotionEvent;
 import android.view.View;
 
 /**
@@ -15,6 +17,13 @@ public class myView extends View {
     public myView(Context context, AttributeSet attrs) {
         super(context, attrs);
     setBackgroundColor(Color.BLACK);
+
+    setOnClickListener(new OnClickListener() {
+        @Override
+        public void onClick(View v) {
+            Log.v("shine","onclick");
+        }
+    });
     }
 
     @Override
@@ -23,5 +32,13 @@ public class myView extends View {
         Paint paint = new Paint();
         paint.setColor(Color.YELLOW);
         canvas.drawCircle(100,100,40,paint);
+        canvas.drawLine(0,0,200,200,paint);
+
+    }
+
+    @Override
+    public boolean onTouchEvent(MotionEvent event) {
+        Log.v("shine","onTouchEvent");
+        return super.onTouchEvent(event);
     }
 }
